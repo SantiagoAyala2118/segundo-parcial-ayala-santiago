be3daaf7fb2d1e9e68cf616bb7ac848ef060c020
Santiago Ayala <santitomayala07@gmail.com>
2025-06-27 10:28:20 -0300

    Se creó el modelo Movie en el archivo movie.model.js

---
+++ src/models/movie.model.js
@@ -0,0 +1,11 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Movie = sequelize.define('Movie', {
+    title: { type: DataTypes.STRING(), allowNull:false, unique: true },
+    director: { type: DataTypes.STRING(), allowNull: false },
+    duration: { type: DataTypes.INTEGER(), allowNull: false },
+    genre: { type: DataTypes.STRING(), allowNull: false },
+    description: { type: DataTypes.TEXT() }
+
+});
