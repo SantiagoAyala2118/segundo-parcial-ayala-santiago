4ac933ac8b882af1ab2129b545104d331f25dd1e
Santiago Ayala <santitomayala07@gmail.com>
2025-06-27 10:50:38 -0300

    Se completó el controlador para crear una pelicula

---
+++ src/controllers/movie.controllers.js
@@ -0,0 +1,112 @@
+import { Movie } from '../models/movie.model.js';
+
+export const createMovie = async (req, res) => {
+    try {
+        const { title, director, duration, genre, description } = req.body;
+        if(!title || !director || !duration || !genre){
+            return res.status(400).json({
+                message: 'No pueden haber campos nulos'
+            });
+        };
+
+        if(title==="" || director ==="" || duration==="" || genre===""){
+            return res.status(400).json({
+                message: 'Los campos no pueden ser espacion vacíos'
+            });
+        };
+
+        const existingMovie = await Movie.findOne({
+            where: {
+                title
+            }
+        });
+        if(existingMovie){
+            return res.status(400).json({
+                message: 'No pueden haber peliculas repetidas'
+            });
+        };
+
+        if(typeof title !== "string"){
+            return res.status(400).json({
+                message: 'El titulo debe ser un valor string'
+            });
+        };
+
+        if(typeof director != "string" || typeof genre != "string" || typeof description != 'string'){
+            return res.status(400).json({
+                message: 'Todos los campos excepto duration deben ser de tipo string (character)'
+            })
+        }
+
+
+        const movie = await Movie.create({
+            title,
+            director,
+            duration,
+            genre,
+            description
+        });
+        res.status(201).json({
+            message: 'Peliula creada con exito',
+            movie
+        });
+
+    } catch (err) {
+        console.error('Error interno del servidor al intentar crear un personaje')
+        return res.status(500).json({
+            message: 'Error del servidor'
+        });
+    };
+};
+
+
+
+export const getAllMoviesMovie = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error('Error interno del servidor al intentar traer los personajes')
+        return res.status(500).json({
+            message: 'Error del servidor'
+        });
+    };
+};
+
+
+
+export const getOneMovieMovie = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error('Error interno del servidor al intentar traer un personaje')
+        return res.status(500).json({
+            message: 'Error del servidor'
+        });
+    };
+};
+
+
+
+export const updateMovie = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error('Error interno del servidor al intentar actualizar un personaje')
+        return res.status(500).json({
+            message: 'Error del servidor'
+        });
+    };
+};
+
+
+
+export const deleteMovie = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error('Error interno del servidor al intentar borrar un personaje')
+        return res.status(500).json({
+            message: 'Error del servidor'
+        });
+    };
+};
